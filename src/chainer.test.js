7fe9178d1d886cb12dec413aead9086c91c71fbc
Hanna Svirina <26anna07@ukr.net>
2025-04-15 15:13:54 +0200

    solution

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -18,7 +18,7 @@ describe('chainer', () => {
   it('should work correctly if value is present', () => {
     const chained = chainer([f1, f2, f3]);
 
-    expect(chained(1)).toBe(16);
+    expect(chained(1)).toBe(6);
   });
 
   it('should return default value, if functions is empty', () => {
