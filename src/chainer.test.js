0b5e2be8be763d15cb162fa28ef9e68a1e403878
Hanna Svirina <26anna07@ukr.net>
2025-04-15 15:10:38 +0200

    solution

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,3 +1,29 @@
-'use strict'
+'use strict';
 
 const { chainer } = require('./chainer');
+
+describe('chainer', () => {
+  function f1(x) {
+    return x * 2;
+  };
+
+  function f2(x) {
+    return x + 2;
+  };
+
+  function f3(x) {
+    return Math.pow(x, 2);
+  };
+
+  it('should work correctly if value is present', () => {
+    const chained = chainer([f1, f2, f3]);
+
+    expect(chained(1)).toBe(16);
+  });
+
+  it('should return default value, if functions is empty', () => {
+    const chained = chainer([]);
+
+    expect(chained(4)).toBe(4);
+  });
+});
